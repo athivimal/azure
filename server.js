69dd99349c80caebf8b2a1e2e805d1c11faa052f
athivimal <46685394+athivimal@users.noreply.github.com>
2020-11-20 20:23:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const WebSocket = require('ws');
 const path = require('path');
 const EventHubReader = require('./scripts/event-hub-reader.js');
 
-const iotHubConnectionString = "HostName=msmeiothub123.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=RnB7f44/lpCy30SMYM2/mlVyFmfaNyCtuQasoYW8lfM=";
+const iotHubConnectionString = "HostName=KTBAGRI.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=AaYvkN3lA+HBgoiWn5byRFG6KlIE3izDdoe2pljI8d8=";
 
-
-const eventHubConsumerGroup = "FC";
+const eventHubConsumerGroup = "fc";
 
 
 // Redirect requests to the public subdirectory to the root
